d38ce31ff629bc20e6c883b959ccbead83862da2
mriansabado <djian808@gmail.com>
2022-06-25 23:56:45 -0700

    re-establishing connection with repo

---
+++ testing-vue-project/src/components/MyForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <input type="text" placeholder="Name" />
+    </div>
+    <div>
+        <button>Submit</button>
+    </div>
+</template>
+
+<script></script>
+
+<style></style>
